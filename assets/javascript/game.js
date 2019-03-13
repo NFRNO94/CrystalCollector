007120f37dc01092d37b14709eb7d91e18abd741
NFRNO <NFRNO@Dantes-MacBook-Pro.local>
2019-03-13 01:15:48 -0700

    this is my third commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,51 +1,83 @@
-// number to guess
-var targetNumber = 50;
-//win and loss variable counters
-var winCount = 0;
-var lossCount = 0;
+$(document).ready(function () {
+    //set variable to select a random number between 18-120 for the number to guess
+    var increment = 18 + Math.round(Math.random() * 120);
+    // show number in HTML
+    $("#solution-number").html(increment);
+    console.log(increment);
 
-// show solution number in html
-$("#solution-number").text(targetNumber);
+    //create variables for gems to select a random number for each gem
+    var clearGem = 1 + Math.floor(Math.random() * 12);
+    var redGem = 1 + Math.floor(Math.random() * 12);
+    var greenGem = 1 + Math.floor(Math.random() * 12);
+    var blueGem = 1 + Math.floor(Math.random() * 12);
 
-//create a counter for the crystal click values
-var counter = 0;
+    // declare your total, win and loss variable counters, and set to 0
+    var yourTotal = 0;
+    var winCount = 0;
+    var lossCount = 0;
+    //write to html
+    $("#your-score").html(yourTotal);
+    $("#wins-count").html(winCount);
+    $("#losses-count").html(lossCount);
 
-//create array with numbers for the crystals to be randomly assigned
-var numberOptions = [10, 11, 1, 5];
-
-//set variable to randomly assign the numbers in the array to the 
-//crystal images
-var increment = numberOptions[Math.round(Math.random() * numberOptions.length)];
-
-// create on click event for when the crystal images are clicked
-$(".crystal-image").on("click", function () {
-
-    //the counter will equal the random number each click generates
-    counter += increment;
-    console.log(counter);
-
-    //working on getting result to show up in wins/losses/current score
-    //$("your-score").append("Number Guessed: " + counter);
-
-    //if added for wins condition
-    if (counter === targetNumber) {
-        winCount++;
-        alert("You win!");
-        console.log("You win! " + winCount);
-        $("wins-count").prepend(winCount);
-
+    // create var for reset
+    var reset = function () {
+        yourTotal = 0;
+        increment = 18 + Math.floor(Math.random() * 120);
+        clearGem = 1 + Math.floor(Math.random() * 12);
+        redGem = 1 + Math.floor(Math.random() * 12);
+        greenGem = 1 + Math.floor(Math.random() * 12);
+        blueGem = 1 + Math.floor(Math.random() * 12);
+        console.log(increment);
+        $("#solution-number").html(increment);
+        $("#your-score").html(yourTotal);
+        $("#wins-count").html(winCount);
+        $("#losses-count").html(lossCount);
     }
 
-    // else if added for losses condition
-    else if (counter >= targetNumber) {
-        lossCount++;
-        //alerts loss
-        alert("You lose!!");
+    var solutionCheck = function () {
+        //win condition
+        if (yourTotal == increment) {
+            winCount++;
+            alert("You win!");
+            $("wins-count").html(winCount);
+            reset();
+        }
+        // else if added for losses condition
+        else if (yourTotal > increment) {
+            lossCount++;
+            alert("You lose!!");
+            $("#losses-count").html(lossCount);
+            reset();
+        }
+    };
 
-        console.log("You lose!! " + lossCount);
-        // working on getting result to display in html
-    }
+    $("#crystal1").on("click", function () {
+        yourTotal = yourTotal + clearGem;
+        console.log(yourTotal);
+        $("#your-score").html(yourTotal);
+        solutionCheck();
+    })
 
+    $("#crystal2").on("click", function () {
+        yourTotal = yourTotal + redGem;
+        console.log(yourTotal);
+        $("#your-score").html(yourTotal);
+        solutionCheck();
+    })
+
+    $("#crystal3").on("click", function () {
+        yourTotal = yourTotal + greenGem;
+        console.log(yourTotal);
+        $("#your-score").html(yourTotal);
+        solutionCheck();
+    })
+
+    $("#crystal4").on("click", function () {
+        yourTotal = yourTotal + blueGem;
+        console.log(yourTotal);
+        $("#your-score").html(yourTotal);
+        solutionCheck();
+    })
 
 });
-
